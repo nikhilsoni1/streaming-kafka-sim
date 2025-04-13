4a652781e9212c01cf6b6fbd4987d3e1f3b258b9
nikhil <nikhilkamlesh.soni@gmail.com>
2025-04-12 18:51:11 -0700

    Refactor px4_dbinfo.py: enhance caching logic and remove redundant file

--- polling/px4_dbinfo.py
+++ polling/px4_dbinfo.py
@@ -6,6 +6,8 @@ import os
 import base64
 import redis
 import re
+from uuid import uuid4
+from pprint import pprint
 
 from db.database import SessionLocal
 from db.repositories.dbinfo import RawDbinfoRepository
@@ -32,12 +34,13 @@ def get_current_utc_time():
     current_utc_time_str = current_utc_time.strftime('%Y-%m-%d %H:%M:%S')
     return current_utc_time_str
 
-def get_cached_json(redis_client, url, ttl=3600):
+def get_cached_json(redis_client, url, ttl=28800, bypass_cache=False):
     key = f"url_cache:{hashlib.sha256(url.encode()).hexdigest()}"
-    cached = redis_client.get(key)
-    if cached:
-        print("From cache")
-        return json.loads(cached)
+    if not bypass_cache:
+        cached = redis_client.get(key)
+        if cached:
+            print("From cache")
+            return json.loads(cached)
     print("Fetching from URL...")
     resp = requests.get(url)
     resp.raise_for_status()
@@ -45,37 +48,35 @@ def get_cached_json(redis_client, url, ttl=3600):
     redis_client.setex(key, ttl, json.dumps(json_data))
     return json_data
 
-def fetch_and_store(redis_client, url, ttl=3600):
+def fetch_and_store(redis_client, url, ttl=36000, **kwargs):
+    bypass_cache = kwargs.get('bypass_cache', False)
+    print(f"Fetching data from API with Redis cache... {url}")
+    raw_json = get_cached_json(redis_client, url, ttl=ttl, bypass_cache=bypass_cache)
+
+    # Compute hash from the raw JSON
+    raw_json_str = json.dumps(raw_json, sort_keys=True)
+    encoded_json = base64.b64encode(raw_json_str.encode("utf-8")).decode("utf-8")
+    # hash_sha256 = hashlib.sha256(encoded_json.encode("utf-8")).hexdigest()
+    # Clean the raw JSON before storing
+    cleaned_json = replace_control_chars(raw_json)
+    log_ts_utc = get_current_utc_time()
+    job_id = str(uuid4())
+    records_list = list()
     session = SessionLocal()
-    try:
-        print(f"Fetching data from API with Redis cache... {url}")
-        raw_json = get_cached_json(redis_client, url, ttl=ttl)
+    repo = RawDbinfoRepository(session=session)
+    for ctr, record in enumerate(cleaned_json):
+        record['job_id'] = job_id
+        record['log_ts_utc'] = log_ts_utc
+        records_list.append(record.copy())
+    repo.write_records(records_list, use_tqdm=True)
+    session.close()
+    print(f"Fetched {len(records_list)} records.")
 
-        # Compute hash from the raw JSON
-        raw_json_str = json.dumps(raw_json, sort_keys=True)
-        encoded_json = base64.b64encode(raw_json_str.encode("utf-8")).decode("utf-8")
-        hash_sha256 = hashlib.sha256(encoded_json.encode("utf-8")).hexdigest()
+        
+        
 
-        # Clean the raw JSON before storing
-        cleaned_json = replace_control_chars(raw_json)
 
-        # Insert into DB using repository
-        repo = RawDbinfoRepository(session)
-        repo.insert(
-            log_ts_utc=get_current_utc_time(),
-            hash_sha256=hash_sha256,
-            raw_json=cleaned_json,
-            raw_json_b64=encoded_json,
-        )
-        print("New data inserted successfully.")
-
-    except requests.HTTPError as http_err:
-        print(f"HTTP error: {http_err}")
-    except Exception as err:
-        print(f"Unexpected error: {err}")
-    finally:
-        session.close() 
 if __name__ == '__main__':
     url = "https://review.px4.io/dbinfo"
     redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
-    fetch_and_store(redis_client, url=url, ttl=3600)
+    fetch_and_store(redis_client, url=url, bypass_cache=False)
