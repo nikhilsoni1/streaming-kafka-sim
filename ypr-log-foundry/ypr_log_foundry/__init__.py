754400eb0678b43ff50642cff8b201297f091cbe
Nikhil Soni <nikhilkamlesh.soni@gmail.com>
2025-04-28 14:28:37 -0700

    refactors log foundry to avoid package naming collisions (#11)

--- ypr-log-foundry/ypr_log_foundry/__init__.py
+++ ypr-log-foundry/ypr_log_foundry/__init__.py
@@ -2,6 +2,9 @@ from .parser import ULogParser
 from .parser import apply_typecasts
 from .parser import typecast_dict
 
+__version__ = "0.1.0"
+__author__ = "Nikhil Soni"
+
 __all__ = [
     "ULogParser",
     "apply_typecasts",
