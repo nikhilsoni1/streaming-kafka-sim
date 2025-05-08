1988608d87284e980204375dc6fc5c0e13a419db
Nikhil Soni <nikhilkamlesh.soni@gmail.com>
2025-05-07 19:20:49 -0700

    deprecates monolithic render_rig

--- render-rig/depr_render_rig/README.md
+++
@@ -1,3 +0,0 @@
-## Monolithic Chart Serving
-
-[Prompt](https://chatgpt.com/c/6808578e-6558-8010-bba1-e2d0c460553a)
--- render-rig/depr_render_rig/cache/__init__.py
+++
@@ -1,6 +0,0 @@
-# __init__.py
-from .store import chart_exists
-from .store import save_chart_json
-from .store import get_chart_json
-
-__all__ = ["chart_exists", "save_chart_json", "get_chart_json"]
