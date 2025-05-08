81d8657a786492f2331af394df69ba4744afeb61
Nikhil Soni <nikhilkamlesh.soni@gmail.com>
2025-05-07 19:52:43 -0700

    adds timing to logs when debug logging is on

---
+++ render-rig/render_rig2/utils/timing.py
@@ -0,0 +1,14 @@
+from contextlib import contextmanager
+from time import perf_counter
+from render_rig2.utils.logger import logger
+from render_rig2.utils.logger import is_debug_enabled
+
+@contextmanager
+def timed_debug_log(message: str):
+    if is_debug_enabled():
+        start = perf_counter()
+        yield
+        duration = round(perf_counter() - start, 2)
+        logger.debug(f"{message} took {duration}s")
+    else:
+        yield
--- render-rig/samples/check_cache.py
+++ render-rig/samples/check_cache.py
@@ -4,4 +4,3 @@ print("📦 Cached keys:")
 for key in cache.iterkeys():
     print("-", key)
 cache.clear()
-print("flight-px4-logs/raw-logs/2025/4/8/file_64c166b0dd104e44bd81adb9bddfb5bb.ulg" in cache)
