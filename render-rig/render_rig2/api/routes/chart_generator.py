1bddd4158aa59eba8736c666ff66fd50306ee696
Nikhil Soni <nikhilkamlesh.soni@gmail.com>
2025-05-08 19:03:59 -0700

    Extends render rig2 (#21)
    * adds storage and db logging for new chart generation
    
    * adds delay between chart generator calls and increases iteration count
    
    * black formatting

--- render-rig/render_rig2/api/routes/chart_generator.py
+++ render-rig/render_rig2/api/routes/chart_generator.py
@@ -4,6 +4,8 @@ from render_rig2.tasks import lookup_log_registry
 from render_rig2.tasks import lookup_chart_registry
 from render_rig2.tasks import get_existing_chart
 from render_rig2.tasks import get_log_dispatch_chart
+from render_rig2.tasks import log_chart_in_registry
+from render_rig2.tasks import store_chart_json_in_s3
 from render_rig2.utils.logger import logger
 from time import perf_counter
 
@@ -12,6 +14,7 @@ router = APIRouter()
 
 @router.get("/{log_id}/{chart_name}")
 async def generate_chart(log_id: str, chart_name: str):
+    # double check chart_json type
     """
     Generate a chart for a given log_id and chart_name.
     """
@@ -48,9 +51,18 @@ async def generate_chart(log_id: str, chart_name: str):
 
     if chart_data is not None:
         logger.success(f"Chart generated from log returned in {et2:.2f} seconds")
+        post_pipeline = chain(
+            store_chart_json_in_s3.s(log_id, chart_name, chart_data),
+            log_chart_in_registry.s(),
+        )
+        post_pipeline.apply_async()
         return {"status": "generated", "data": chart_data}
+    # run pipeline3 aafter sending the result above
 
     logger.error(
         f"Failed to find or generate a chart: {chart_name} for log_id: {log_id}, total time spent: {et1 + et2:.2f} seconds"
     )
     return {"status": None, "data": chart_data}
+
+
+#   after return to fastapi, store in s3 and update the chart registry
---
+++ render-rig/samples/test_store_chart_in_s3.py
@@ -0,0 +1,21 @@
+from render_rig2.tasks.store_chart_json_in_s3 import store_chart_json_in_s3
+import uuid
+import datetime
+import json
+from pprint import pprint
+
+log_id = f"{uuid.uuid4()}"
+log_id = "025468ef-81b7-4ec6-87a4-19a323b77b17"
+chart_name = f"del_this_{uuid.uuid4().hex}"
+chart_json = {
+    "log_id": log_id,
+    "chart_name": chart_name,
+    "log_ts_utc": datetime.datetime.now(datetime.timezone.utc).strftime(
+        "%Y-%m-%d %H:%M:%S"
+    ),
+}
+chart_json = json.dumps(chart_json)
+result = store_chart_json_in_s3(
+    log_id=log_id, chart_name=chart_name, chart_json=chart_json
+)
+pprint(result)
