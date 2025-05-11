f544cf5694630f38dc2344747afc436fd9583628
Nikhil Soni <nikhilkamlesh.soni@gmail.com>
2025-05-11 00:58:27 -0700

    feat: add internet gateway and public route configuration in Terraform

--- render-rig/infrastructure/networking/main.tf
+++ render-rig/infrastructure/networking/main.tf
@@ -34,3 +34,19 @@ resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.render_rig2_api_subnet.id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_internet_gateway" "render_rig2_igw" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name    = "render_rig2_igw"
+    service = "render_rig2"
+  }
+
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.render_rig2_igw.id
+}
--- render-rig/infrastructure/networking/outputs.tf
+++ render-rig/infrastructure/networking/outputs.tf
@@ -7,3 +7,8 @@ output "render_rig2_worker_subnet_id" {
   description = "ID of the private subnet used by the worker"
   value       = aws_subnet.render_rig2_worker_subnet.id
 }
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.render_rig2_igw.id
+}
