18574c03a1020d8b4c8cc0d360744eb90e4bd3c5
Joel Dodich <88948803+joeldodich@users.noreply.github.com>
2025-05-13 11:36:24 -0500

    cleanup

--- services/graphql/codegen.ts
+++ services/graphql/codegen.ts
@@ -4,12 +4,12 @@ const config: CodegenConfig = {
   schema: "./*.graphql",
   generates: {
     "./src/__generated__/resolvers-types.ts": {
-      config: {
-        federation: true,
-        useIndexSignature: true,
-        contextType: '../types/DataSourceContext#DataSourceContext',
+      // config: {
+      //   federation: true,
+      //   useIndexSignature: true,
+      //   contextType: '../types/DataSourceContext#DataSourceContext',
         
-      },
+      // },
       plugins: ["typescript","typescript-resolvers"]
     },
   },
