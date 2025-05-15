a96330259bc8dfa5085eb1eeb2f6f6f0255dc42f
Joel Dodich <88948803+joeldodich@users.noreply.github.com>
2025-05-15 14:24:45 -0400

    read local file

--- services/graphql/src/data-sources/DatabaseApi.ts
+++ services/graphql/src/data-sources/DatabaseApi.ts
@@ -1,3 +1,6 @@
+import path from "path";
+import { Acceleration, VehicleData } from "../__generated__/resolvers-types";
+
 export class DatabaseAPI {
     // private readonly url: string;
     // private readonly auth: string | undefined;
@@ -7,9 +10,32 @@ export class DatabaseAPI {
         // this.auth = process.env.DATABASE_AUTH;
     }
 
+    public async getTimestamps() {
+        const filePath = path.resolve(__dirname, "../../data.json");
+        const { readFile } = await import("fs/promises");
+        const fileContent = await readFile(filePath, { encoding: "utf-8" });
+        const json = JSON.parse(fileContent);
+
+        return json.data.timestamp as VehicleData['timestamp'];
+    }
+
     public async getAccelarationData(args: {
         x: boolean;
         y: boolean;
         z: boolean;
-    }) {}
+    }) {
+        const filePath = path.resolve(__dirname, "../../data.json");
+        const { readFile } = await import("fs/promises");
+        const fileContent = await readFile(filePath, { encoding: "utf-8" });
+        const json = JSON.parse(fileContent);
+
+        // Optionally filter axes based on args
+        let result: Acceleration = {
+            x: args.x ? (json.data.acceleration["x:"] as [string]) : undefined,
+            y: args.y ? (json.data.acceleration["y:"] as [string]) : undefined,
+            z: args.z ? (json.data.acceleration["z:"] as [string]) : undefined,
+        };
+
+        return result;
+    }
 }
