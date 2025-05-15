a4bdb3d7dbf120f6aabe018a14341058236e5908
Joel Dodich <88948803+joeldodich@users.noreply.github.com>
2025-05-15 14:40:54 -0400

    setting up mock resolvers

--- services/graphql/src/resolvers/Query.ts
+++ services/graphql/src/resolvers/Query.ts
@@ -1,6 +1,4 @@
-import { GraphQLResolveInfo } from "graphql";
-import { AccelerationUnits, Resolvers } from "../__generated__/resolvers-types";
-import { DataSourceContext } from "../types/DataSourceContext";
+import { Resolvers } from "../__generated__/resolvers-types";
 
 export const Query: Resolvers = {
     Query: {
@@ -12,49 +10,47 @@ export const Query: Resolvers = {
                 vehicleId: vehicleId,
                 log_id: "fake-log-id",
                 timestamp: await _context.database.getTimestamps(),
-                // data: await _context.database.getAccelarationData({
-                //     x: true,
-                //     y: true,
-                //     z: true,
-                // }),
                 // 'data' will be resolved by the field resolver
             };
         },
     },
-    VehicleData: {
-        async data(parent, _args, _context) {
-            // Here we can resolve the 'data' field if needed
-            // For now, we just return the data as is
-            return parent.data;
-        },
-    },
-    Acceleration: {
-        units(parent: unknown, _args: unknown, _context: unknown) {
-            return AccelerationUnits.Ms2;
-        },
-        async x(parent, _args, _context) {
-            // Assuming getAccelarationData returns an object like { x: string }
-            const data = await _context.database.getAccelarationData({
-                x: true,
-            });
-            return data.x;
-        },
-        async y(parent, _args, _context) {
-            // Assuming getAccelarationData returns an object like { x: string }
-            const data = await _context.database.getAccelarationData({
-                y: true,
-            });
-            return data.y;
-        },
-        async z(parent, _args, _context) {
-            // Assuming getAccelarationData returns an object like { x: string }
-            const data = await _context.database.getAccelarationData({
-                z: true,
-            });
-            return data.z;
-        },
-        __resolveType: function (parent: never, context: DataSourceContext, info: GraphQLResolveInfo): Promise<null> {
-            throw new Error("Function not implemented.");
-        }
-    },
+    // VehicleData: {
+    //     async data(parent, _args, _context) {
+    //         // Assuming getAccelarationData returns an object like { x: string, y: string, z: string }
+    //         const accData = await _context.database.getAccelarationData({
+    //             x: true,
+    //             y: true,
+    //             z: true,
+    //         });
+    //         return {
+    //             acceleration: accData,
+    //         };
+    //     },
+    // },
+    // Acceleration: {
+    //     units(parent, _args, _context) {
+    //         return AccelerationUnits.Ms2;
+    //     },
+    //     async x(parent, _args, _context) {
+    //         // Assuming getAccelarationData returns an object like { x: string }
+    //         const data = await _context.database.getAccelarationData({
+    //             x: true,
+    //         });
+    //         return data.x;
+    //     },
+    //     async y(parent, _args, _context) {
+    //         // Assuming getAccelarationData returns an object like { x: string }
+    //         const data = await _context.database.getAccelarationData({
+    //             y: true,
+    //         });
+    //         return data.y;
+    //     },
+    //     async z(parent, _args, _context) {
+    //         // Assuming getAccelarationData returns an object like { x: string }
+    //         const data = await _context.database.getAccelarationData({
+    //             z: true,
+    //         });
+    //         return data.z;
+    //     },
+    // },
 };
