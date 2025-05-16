3d0e107db320f0e2a1d3d3d97de40552d9e15bb9
Joel Dodich <88948803+joeldodich@users.noreply.github.com>
2025-05-16 10:12:10 -0400

    mock data server working

---
+++ services/realtime-db/mocks/data-service/index.ts
@@ -0,0 +1,31 @@
+import { faker } from "@faker-js/faker";
+import express from "express";
+import _ from "lodash";
+
+const app = express();
+
+app.get("/address", (req, res) => {
+    const count = req.query.count;
+    if (!count) {
+        return res
+            .status(400)
+            .send({ errorMsg: "count query parameter is missing." });
+    }
+    res.send(
+        _.times(count, () => {
+            const address = faker.address;
+            return {
+                country: address.country(),
+                city: address.city(),
+                state: address.state(),
+                zipCode: address.zipCode(),
+                latitude: address.latitude(),
+                longitude: address.longitude(),
+            };
+        })
+    );
+});
+
+app.listen(3030, () => {
+    console.log("server started on port 3030");
+});
