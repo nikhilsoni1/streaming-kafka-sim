4606fd4117af04e7023fe5c289c320aab0023313
Nikhil Soni <nikhilkamlesh.soni@gmail.com>
2025-05-22 16:18:38 -0700

    Chart power (#28)
    * refactor: replace deprecated imports and streamline log data retrieval
    
    * adds chart_power

---
+++ render-rig/render_rig2/chart_engine/charts/chart_power.py
@@ -0,0 +1,95 @@
+# chart_power.py
+
+from render_rig2.chart_engine.base import Chart
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from render_rig2.utils.logger import logger
+
+
+class ChartPower(Chart):
+    chart_name = "chart_power"
+    topic_name = "battery_status"
+
+    def is_topic_available(self, log_data: dict) -> bool:
+        if self.topic_name in log_data.list_topics():
+            return True
+        return False
+
+    def generate(self, log_data: dict):
+        df = log_data.get_topic_df(self.topic_name)
+        fig = make_subplots(
+            rows=3,
+            cols=1,
+            shared_xaxes=True,
+            subplot_titles=("Voltage & Current", "Battery Status", "Internal Estimates"),
+            vertical_spacing=0.05,
+        )
+
+        # First subplot: Voltage and Current
+        fig.add_trace(
+            go.Scatter(
+                x=df["timestamp"].tolist(),
+                y=df["voltage_v"].tolist(),
+                mode="lines",
+                name="Battery Voltage [V]",
+            ), row=1, col=1
+        )
+        fig.add_trace(
+            go.Scatter(
+                x=df["timestamp"].tolist(),
+                y=df["current_a"].tolist(),
+                mode="lines",
+                name="Battery Current [A]",
+            ), row=1, col=1
+        )
+
+        # Second subplot: Battery discharged and remaining
+        fig.add_trace(
+            go.Scatter(
+                x=df["timestamp"].tolist(),
+                y=df["discharged_mah"].divide(100).round(2).tolist(),
+                mode="lines",
+                name="Discharged Amount [mAh / 100]",
+            ), row=2, col=1
+        )
+        fig.add_trace(
+            go.Scatter(
+                x=df["timestamp"].tolist(),
+                y=df["remaining"].multiply(100).round(2).tolist(),
+                mode="lines",
+                name="Battery remaining [0=empty, 10=full]",
+            ), row=2, col=1
+        )
+
+        # Third subplot: OCV estimate and Internal resistance
+        is_ocv_estimate_available = "ocv_estimate" in df.columns
+        is_internal_resistance_estimate_available = "internal_resistance_estimate" in df.columns
+        if is_ocv_estimate_available and is_internal_resistance_estimate_available:
+            fig.add_trace(
+                go.Scatter(
+                    x=df["timestamp"].tolist(),
+                    y=df["ocv_estimate"].tolist(),
+                    mode="lines",
+                    name="OCV Estimate [V]",
+                ), row=3, col=1
+            )
+            fig.add_trace(
+                go.Scatter(
+                    x=df["timestamp"].tolist(),
+                    y=df["internal_resistance_estimate"].multiply(1000).round(2).tolist(),
+                    mode="lines",
+                    name="Internal Resistance Estimate [mOhm]",
+                ), row=3, col=1
+            )
+
+            fig.update_layout(
+                title_text="Power",
+                height=700,
+                showlegend=True,
+                legend=dict(orientation='h', yanchor='bottom', y=1.02, xanchor='right', x=1),
+            )
+        else:
+            logger.info(
+                f"OCV estimate, available: {is_ocv_estimate_available} or Internal resistance estimate, available: {is_internal_resistance_estimate_available}."
+            )
+        return fig
--- render-rig/samples/render_chart_acceleration_raw_xyz.py
+++ render-rig/samples/render_chart_acceleration_raw_xyz.py
@@ -1,29 +1,22 @@
 # render_accel_chart.py
+from render_rig2.tasks.get_log_dispatch_chart import get_existing_log
+from render_rig2.tasks.lookup_log_registry import lookup_log_registry
+from render_rig2.chart_engine.charts import ChartAccelRawXYZ
 
-from depr_render_rig import get_log_data
-from depr_render_rig import ChartAccelRawXYZ
-from depr_render_rig.utils import hash_bytes_sha256
-from depr_render_rig.data_access.database.session_manager import (
-    get_log_registry_session,
-)
-from depr_render_rig.data_access.object_access.session_manager import (
-    create_boto3_client,
-)
 
-s3_client = create_boto3_client("s3", region_name="us-east-1", max_retries=3)
-db_log_registry_session = get_log_registry_session()
-# Load and parse the .ulg file
-log_data = get_log_data(
-    "1fc1b7b4-a68a-491b-8984-3234ed71be08", db_log_registry_session, s3_client
-)
+log_id = "1fc1b7b4-a68a-491b-8984-3234ed71be08"
+payload = lookup_log_registry(log_id)
+_log_id, bucket_name, key = payload
+assert _log_id == log_id, f"Log ID mismatch: {log_id} != {_log_id}"
+result = get_existing_log(bucket_name, key)
+print(result)
+
+
 
 # Generate the chart
 chart = ChartAccelRawXYZ()
-if chart.is_topic_available(log_data):
-    fig = chart.generate(log_data)
-    fig_json = fig.to_json().encode("utf-8")
-    fig_hash_sha256 = hash_bytes_sha256(fig_json)
-    print(f"Chart hash: {fig_hash_sha256}")
+if chart.is_topic_available(result):
+    fig = chart.generate(result)
     fig.show()  # Opens in browser or notebook
 else:
     print("sensor_combined topic not found in this log.")
