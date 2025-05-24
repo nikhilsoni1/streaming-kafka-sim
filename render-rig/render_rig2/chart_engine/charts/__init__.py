0785850db69aa81aad6770bb462b87de27c9730e
Nikhil Soni <nikhilkamlesh.soni@gmail.com>
2025-05-24 13:21:19 -0700

    Chart vibration metrics (#29)
    * feat: add async API call and chart generation functionality; remove deprecated chart scripts
    
    * adds chart_vibration_metrics

--- render-rig/render_rig2/chart_engine/charts/__init__.py
+++ render-rig/render_rig2/chart_engine/charts/__init__.py
@@ -1,7 +1,9 @@
 from .chart_acceleration_raw_xyz import ChartAccelRawXYZ
 from .chart_power import ChartPower
+from .chart_vibration_metrics import ChartVibrationMetrics
 
 CHART_REGISTRY = {
     "chart_accel_raw_xyz": ChartAccelRawXYZ,
     "chart_power": ChartPower,
+    "chart_vibration_metrics": ChartVibrationMetrics,
 }
