7fa3ed6d81c990bec634264043efb26b7840e12c
Nikhil Soni <nikhilkamlesh.soni@gmail.com>
2025-05-24 20:07:37 -0700

    adds chart_velocity (#30)

--- render-rig/render_rig2/chart_engine/charts/__init__.py
+++ render-rig/render_rig2/chart_engine/charts/__init__.py
@@ -1,9 +1,11 @@
 from .chart_acceleration_raw_xyz import ChartAccelRawXYZ
 from .chart_power import ChartPower
 from .chart_vibration_metrics import ChartVibrationMetrics
+from .chart_velocity import ChartVelocity
 
 CHART_REGISTRY = {
     "chart_accel_raw_xyz": ChartAccelRawXYZ,
     "chart_power": ChartPower,
     "chart_vibration_metrics": ChartVibrationMetrics,
+    "chart_velocity": ChartVelocity,
 }
